ca43fc190fbb869c626c613d603129032b8e3daf
Dtober5 <dtober5@alamo.edu>
2024-09-05 21:20:00 -0500

    first

---
+++ script.js
@@ -0,0 +1,22 @@
+var gn = 20; //global
+const result= 28;
+{ //block
+    let n1 = 2;
+    let n2 = 4;
+
+    let result = gn + (n1*n2);
+    var resulttext = "The equation of 20+(2*4) is: "; //string
+}
+
+console.log(resulttext + result)
+
+// my beautiful car
+const car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2017,
+};
+// new property
+car.color = "Maroon";
+console.log(car);
+
